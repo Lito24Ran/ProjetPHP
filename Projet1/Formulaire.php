62ebe63242d9f68ffc98d6cbf3afb5a279bf1eba
Victorio178 <victoriorandriamanalina@gmail.com>
2025-07-06 10:18:48 -0700

    nanisy id anle post (tsaiko ra mety)

--- Projet1/Formulaire.php
+++ Projet1/Formulaire.php
@@ -7,9 +7,10 @@ if ($conn->connect_error) {
 // on recupere les données du formulaire
 $nom = $_POST['nom'];
 $commentaire = $_POST['commentaire'];
+$post_id = $_POST['post_id'];
 
 // 3. Requête SQL pour l'insetion
-$sql = "INSERT INTO commentaire (nom, commentaire) VALUES (?, ?)";
+$sql = "INSERT INTO commentaire (nom, commentaire, post_id) VALUES (?, ?, ?)";
 
 // Preparation de la requête
 $stmt = $conn->prepare($sql);
@@ -20,7 +21,7 @@ if (!$stmt) {
 }
 
 // 4. Liaison des paramètres et exécution
-$stmt->bind_param("ss", $nom, $commentaire);
+$stmt->bind_param("ss", $nom, $commentaire, $post_id);
 $stmt->execute();
 
 echo "Commentaire enregistré avec succès.<br>";
