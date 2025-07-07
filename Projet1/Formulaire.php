5287cd3f8e0185cfaadb5ffc00bdbba79bd0630a
Victorio178 <victoriorandriamanalina@gmail.com>
2025-07-07 10:45:14 -0700

    changement kely

--- Projet1/Formulaire.php
+++ Projet1/Formulaire.php
@@ -10,7 +10,7 @@ $commentaire = $_POST['commentaire'];
 $post_id = $_POST['post_id'];
 
 // 3. Requête SQL pour l'insetion
-$sql = "INSERT INTO commentaire (nom, commentaire, post_id) VALUES (?, ?, ?)";
+$sql = "INSERT INTO commentaires (nom, commentaire, post_id) VALUES (?, ?, ?)";
 
 // Preparation de la requête
 $stmt = $conn->prepare($sql);
@@ -21,11 +21,14 @@ if (!$stmt) {
 }
 
 // 4. Liaison des paramètres et exécution
-$stmt->bind_param("ss", $nom, $commentaire, $post_id);
-$stmt->execute();
+$stmt->bind_param("ssi", $nom, $commentaire, $post_id);
+if ($stmt->execute()) {
 
 echo "Commentaire enregistré avec succès.<br>";
 echo '<a href="./Home.html">Retour</a>';
+} else {
+    echo "Erreur lors de l'enregistrement : " . $stmt->error;
+}
 
 // rupture
 $stmt->close();
